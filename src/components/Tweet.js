9b66c7ad5dc22c51c41fb1e66c374482b742b1b1
Ashley <ashley.ratigan@gmail.com>
2018-10-30 16:07:39 -0400

    add boilerplate files for components
    Co-authored-by: River Magnetic <34926618+RiverMagnetic@users.noreply.github.com>

---
+++ src/components/Tweet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tweet = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Tweet
---
+++ src/components/TweetList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TweetList = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default TweetList
